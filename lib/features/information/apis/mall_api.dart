5e5a19c48ff76447859ebd264755ad2f1343e984
Montell Muturi <muturimonty@gmail.com>
2022-12-22 15:02:40 +0300

    created api hooks from malls and stores

--- lib/features/information/apis/mall_api.dart
+++ lib/features/information/apis/mall_api.dart
@@ -1,16 +1,27 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:wayo/locator.dart';
 
 import '../models/mall.dart';
 import '../models/store.dart';
 
 class MallApi {
   Future<List<Mall>> getMalls() async {
-    try {} on DioError catch (e) {
+    final malls = <Mall>[];
+    try {
+      final dio = locator.get<Dio>();
+      final response = await dio.get('/malls');
+      if (response.statusCode == 200) {
+        for (Map<String, dynamic> element in response.data) {
+          malls.add(Mall.fromJson(element));
+        }
+        return malls;
+      }
+    } on DioError catch (e) {
       log(e.message);
     }
-    return [];
+    return malls;
   }
 
   Future<List<Store>> getStoresInMall(String mallId) async {
--- lib/features/information/apis/store_api.dart
+++ lib/features/information/apis/store_api.dart
@@ -1,15 +1,27 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:wayo/locator.dart';
 
 import '../models/store.dart';
 
 class StoreApi {
   Future<List<Store>> getStores() async {
-    try {} on DioError catch (e) {
+    final malls = <Store>[];
+    try {
+      final dio = locator.get<Dio>();
+      final response = await dio.get('/stores');
+      if (response.statusCode == 200) {
+        print(response.data);
+        for (Map<String, dynamic> element in response.data) {
+          malls.add(Store.fromJson(element));
+        }
+        return malls;
+      }
+    } on DioError catch (e) {
       log(e.message);
     }
-    return [];
+    return malls;
   }
 
   Future<Store?> getStore(String mallId) async {
