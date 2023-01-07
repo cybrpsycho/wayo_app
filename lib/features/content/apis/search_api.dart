640321cbc9f73d802e2b280519743ffbc0d140a4
Montell Muturi <muturimonty@gmail.com>
2023-01-07 18:00:51 +0300

    added search api

---
+++ lib/features/content/apis/search_api.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+
+class SearchApi {
+  Future<dynamic> searchForItem(String query) async {
+    final dio = Dio();
+
+    dio.options.headers = {
+      'Content-Type': 'application/json',
+      'X-TYPESENSE-API-KEY': 'txrKb54EtuRQD4OtpN41gPH7hmVFyNeW'
+    };
+
+    const hostName = 'wayo.site';
+    const path = '/collections/data/documents/search';
+    final queryParams = {'q': query, 'query_by': 'name,address'};
+
+    final uri = Uri.https(hostName, path, queryParams);
+
+    final response = await dio.get(uri.toString());
+
+    return response.data;
+  }
+}
